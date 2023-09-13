e83440c3397f5a1a881a992cbfe8f9ff19e57413
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-13 16:24:05 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,15 +1,28 @@
+let eyewidth = 150;
+let facewidth = 300;
+
 function setup() {
     createCanvas (400,600);
-    background (255, 0, 255);
+    background (0, 0, 0);
 }
 
 function draw() {
-    stroke(0);
-   fill ("orange");
-   ellipse (200, 300,100,100);
-   fill(0, 255, 0);
-   ellipse (300, 300, 200, 200);
-   stroke(255, 0, o);
-   strokeweight(4);
+   fill ("white");
+   ellipse (100, 300, eyewidth, eyewidth);
+   ellipse (300, 300, eyewidth, eyewidth);
+   fill ("white");
+   stroke(0);
+  ellipse (200, 400, facewidth, facewidth);
+  stroke(0);
+   fill ("black");
+  circle(120, 340, 75);
+  circle(260, 340, 75);
+  fill ("white");
+  circle(140, 360, 30);
+  circle(280, 360, 30);
+  circle(120, 340, 20);
+  circle(260, 340, 20);
+  fill ("pink");
+  rect(150, 450, 100, 50, 20);
 }
 
