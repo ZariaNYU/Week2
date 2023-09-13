761db4016e3d8c87b035c11fd84116c7ddfd20d7
ZariaNYU <144282037+ZariaNYU@users.noreply.github.com>
2023-09-13 15:37:13 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,5 +1,15 @@
 function setup() {
+    createCanvas (400,600);
+    background (255, 0, 255);
 }
 
 function draw() {
+    stroke(0);
+   fill ("orange");
+   ellipse (200, 300,100,100);
+   fill(0, 255, 0);
+   ellipse (300, 300, 200, 200);
+   stroke(255, 0, o);
+   strokeweight(4);
 }
+
